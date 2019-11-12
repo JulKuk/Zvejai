0c835eda38b098239a38e037f00d8872f7e76fda
marvei <martynas.veikutis@ktu.edu>
2019-11-12 22:09:08 +0200

    Facade class created for testing and database operations.

---
+++ GameServer/Models/Facade/DBController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameServer.Models.Facade
+{
+    public class DBController
+    {
+        //cia galima su facade padaryt viska kad su duombaze bendrautu
+        //kol kas tik dar niekas nejuda
+    }
+}
---
+++ GameServer/Models/Facade/TestingFacade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameServer.Models.Facade
+{
+    public class TestingFacade
+    {
+        //cia susideti tuos testus kur destytojui rodem
+    }
+}
