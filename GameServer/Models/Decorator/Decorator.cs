d88b428d3e02df47ef6feaab9d34aea968e251ca
Julius <jku@pvcase.com>
2019-11-17 22:55:46 +0200

    Belekas

---
+++ GameServer/Models/Decorator/Decorator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameServer.Models;
+
+namespace GameServer.Models.Decorator
+{
+    public abstract class Decorator : Ginklas
+    {
+        public Decorator(Player player/*, Ginklas inner*/) : base(player)
+        {
+            //this.innerElement = inner;
+        }
+
+        //private Ginklas innerElement;
+
+        public sealed override string Show()
+        {
+            return "Decorator object: " + specificAction() /*+ " ant " + this.innerElement.Show() */+ "; Player name: " + player.Name;
+        }
+
+        public abstract string specificAction();
+
+    }
+}
---
+++ GameServer/Models/Decorator/Ginklas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameServer.Models;
+
+namespace GameServer.Models.Decorator
+{
+    public abstract class Ginklas
+    {
+        protected Player player;
+
+        public Ginklas(Player player)
+        {
+            this.player = player;
+        }
+
+        public abstract string Show();
+
+        public abstract Boolean canShow();
+    }
+}
