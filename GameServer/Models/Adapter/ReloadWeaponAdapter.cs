3cefb6b2d15db7907c9b836d723ecd61c6db03c5
Julius <jku@pvcase.com>
2019-12-15 13:05:40 +0200

    ADapter

---
+++ GameServer/Models/Adapter/ReloadWeaponAdapter.cs
@@ -0,0 +1,17 @@
+﻿using GameServer.Models.State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameServer.Models.Adapter
+{
+    public class ReloadWeaponAdapter: IPlayerState
+    {
+
+        public void changePlayerState(Player player)
+        {
+            player.Weapon._kiekKulkuYra = player.Weapon.ammo;
+        }
+    }
+}
